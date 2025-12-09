943de8bfa1c456fbc342b1ebad2e58fd12d84951
gxuamethyst <gxuamethyst@gmail.com>
2025-12-09 22:26:51 +0800

    fix apt mirror config

--- base/src/ubuntu/install/apt_mirror/use_apt_mirror.sh
+++ base/src/ubuntu/install/apt_mirror/use_apt_mirror.sh
@@ -2,8 +2,8 @@
 
 if [ "$USE_APT_MIRROR" = "true" ]; then
     echo "using apt mirror ..."
-    cp -f $INST_SCRIPTS/apt_mirror/source.list /etc/apt/source.list
-    cp -f $INST_SCRIPTS/apt_mirror/source.list.d/kisak-ubuntu-turtle-focal.list.mirror /etc/apt/source.list.d/kisak-ubuntu-turtle-focal.list
+    cp -f $INST_SCRIPTS/apt_mirror/sources.list /etc/apt/sources.list
+    cp -f $INST_SCRIPTS/apt_mirror/sources.list.d/kisak-ubuntu-turtle-focal.list.mirror /etc/apt/sources.list.d/kisak-ubuntu-turtle-focal.list
 else
-    cp -f $INST_SCRIPTS/apt_mirror/source.list.d/kisak-ubuntu-turtle-focal.list /etc/apt/source.list.d/kisak-ubuntu-turtle-focal.list
+    cp -f $INST_SCRIPTS/apt_mirror/sources.list.d/kisak-ubuntu-turtle-focal.list /etc/apt/sources.list.d/kisak-ubuntu-turtle-focal.list
 fi
